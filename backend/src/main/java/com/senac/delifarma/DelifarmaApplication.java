3727a7dd6e1c8a7a50c7d68881f956ba5aa6e161
Gabriel <80397001+gasech@users.noreply.github.com>
2022-10-29 20:34:13 -0300

    First commit backend

---
+++ backend/src/main/java/com/senac/delifarma/DelifarmaApplication.java
@@ -0,0 +1,13 @@
+package com.senac.delifarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelifarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelifarmaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/senac/delifarma/DelifarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.delifarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelifarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
